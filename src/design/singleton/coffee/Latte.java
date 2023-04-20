32f241daf3c4a9f2dc930e24779391fa5a20ca25
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-21 00:15:35 +0900

    [refactor] 싱글톤의 멀티쓰레드를 고려한 synchronized메서드에서 동기화의 중첩으로 인한 시간 낭비를 줄이기 위해 메서드 수정 및 변수에 volatile 키워드 추가

--- src/design/singleton/coffee/Latte.java
+++ src/design/singleton/coffee/Latte.java
@@ -3,15 +3,27 @@ package design.singleton.coffee;
 public class Latte {
     private static final String name = "라떼";
     private static final int price = 2_000;
-    private static  Latte latte = new Latte();
+    private static volatile Latte latte = new Latte();
 
     private Latte(){}
 
-    public static synchronized Latte getInstance() {
+    public static Latte getInstance() {
+        if(latte == null){
+            synchronizeInitialization();
+        }
+        return latte;
+    }
+
+    private static void synchronizeInitialization(){
+        synchronized (Latte.class){
+            initializeLatte();
+        }
+    }
+
+    private static void initializeLatte(){
         if(latte == null){
             latte = new Latte();
         }
-        return latte;
     }
 
     public void tellInfo(){
