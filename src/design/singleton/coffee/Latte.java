1aa9830423c930e5cd2c49a36134c0186259465b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-21 00:22:02 +0900

    [refactor] 최종적으로 holder패턴을 이용해 즉시 초기화를 수행하게 코드 수정

--- src/design/singleton/coffee/Latte.java
+++ src/design/singleton/coffee/Latte.java
@@ -3,27 +3,15 @@ package design.singleton.coffee;
 public class Latte {
     private static final String name = "라떼";
     private static final int price = 2_000;
-    private static volatile Latte latte = new Latte();
 
     private Latte(){}
 
-    public static Latte getInstance() {
-        if(latte == null){
-            synchronizeInitialization();
-        }
-        return latte;
+    public static final class HOLDER{
+        private static final Latte latte = new Latte();
     }
 
-    private static void synchronizeInitialization(){
-        synchronized (Latte.class){
-            initializeLatte();
-        }
-    }
-
-    private static void initializeLatte(){
-        if(latte == null){
-            latte = new Latte();
-        }
+    public static Latte getInstance(){
+        return HOLDER.latte;
     }
 
     public void tellInfo(){
