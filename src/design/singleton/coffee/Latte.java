f0524f3deade6b09b313f90b4c11a6b5ce7596a1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-21 00:02:56 +0900

    [refactor] 지연 초기화를 이용해 싱글톤 패턴 리펙토링

--- src/design/singleton/coffee/Latte.java
+++ src/design/singleton/coffee/Latte.java
@@ -3,11 +3,14 @@ package design.singleton.coffee;
 public class Latte {
     private static final String name = "라떼";
     private static final int price = 2_000;
-    private static final Latte latte = new Latte();
+    private static  Latte latte = new Latte();
 
     private Latte(){}
 
     public static Latte getInstance() {
+        if(latte == null){
+            latte = new Latte();
+        }
         return latte;
     }
 
