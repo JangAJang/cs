91eb8c2540c20284d83bbd6edef58eccb9d3f598
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-04-29 18:45:26 +0900

    [feat] 상품을 추가하거나 제거하고, 총 금액을 알려주는 쇼핑카드 객체 구현

---
+++ src/design/strategy/cart/ShoppingCart.java
@@ -0,0 +1,41 @@
+package design.strategy.cart;
+
+import design.strategy.item.Item;
+
+import java.util.*;
+
+public class ShoppingCart {
+    private final HashMap<Item, Integer> items = new HashMap<>();
+
+    public void addItem(Item item, int amount){
+        if(items.containsKey(item)){
+            int currentAmount = items.get(item);
+            items.replace(item, currentAmount, currentAmount+amount);
+            return;
+        }
+        items.put(item, amount);
+    }
+
+    public void removeItem(Item item, int amount){
+        if(!items.containsKey(item)){
+            throw new IllegalArgumentException("해당 상품을 담은 적 없습니다.");
+        }
+        int stock = items.get(item);
+        if(stock < amount){
+            throw new IllegalArgumentException("해당 상품을 그만큼 담지 않았습니다. 상품 개수 : " + stock);
+        }
+        if(stock == amount){
+            items.remove(item);
+            return;
+        }
+        items.replace(item, stock, stock - amount);
+    }
+
+    public long getTotalPrice(){
+        long total = 0;
+        for(Item each : items.keySet()){
+            total += each.getPrice() * items.get(each);
+        }
+        return total;
+    }
+}
